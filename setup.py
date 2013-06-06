55f075af261cdc568d74d55db6781bd7551db64e
Marc Brinkmann <git@marcbrinkmann.de>
2013-06-06 16:37:52 +0200

    Initial import.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.paht.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='webmonitor',
+    version='0.1dev',
+    description=('Monitors a website by retrieving it periodically, '
+                 'complains if its down.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/webmonitor',
+    license='MIT',
+    py_modules=['webmonitor'],
+    install_requires=['logbook', 'requests'],
+    entry_points={
+        'console_scripts': [
+            'commandname = package:func',
+        ],
+        'gui_scripts': [
+            'commandname = package:func',
+        ]
+    }
+)
